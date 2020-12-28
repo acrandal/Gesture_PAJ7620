4f7bc2952e3070b4841cf64bf95e861b307f032c
Aaron S. Crandall <acrandal@gmail.com>
2020-12-28 00:26:28 -0800

    Small debug in the last merge conflict fixed Versions rev'd to 1.4.0 for next release Plenty of README documentation updates

--- src/RevEng_PAJ7620.h
+++ src/RevEng_PAJ7620.h
@@ -13,7 +13,7 @@
   2020 - PROGMEM code adapted from Jaycar-Electronics' work
   2020 - Modified by Aaron S. Crandall <crandall@gonzaga.edu>
 
-  Version: 1.3.0
+  Version: 1.4.0
 
   Description: This demo can recognize 9 gestures and output the result,
         including move up, move down, move left, move right,
@@ -252,6 +252,7 @@ const unsigned short initCursorRegisterArray[] = {
               //  Where (0,0) is in upper left, positive down (Y) and right (X)
     0x7403,   // Enable cursor mode 0 - gesture, 3 - cursor, 5 - proximity
     0xEF00    // Set Bank 0 (parking it)
+};
 
 /** 
   Gesture result definitions.
